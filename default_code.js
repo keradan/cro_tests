6ee2b2483fa71da18cbc19779b60cf68805877f0
keradan <daniel.kerasidi.w@gmail.com>
2020-09-21 13:30:35 +0300

    test update

---
+++ default_code.js
@@ -0,0 +1,6 @@
+let f_body = 'bGV0IHRlc3RfcGF0aCA9IHRlc3RfaW5mby5jbGllbnQgKyAiLyIgKyB0ZXN0X2luZm8uZmlsZV9uYW1lICsgIi5qcyI7bGV0IHYgPSBNYXRoLmZsb29yKERhdGUubm93KCkgLyAxMDAwKTtsZXQgc2NyaXB0ID0gZG9jdW1lbnQuY3JlYXRlRWxlbWVudCgic2NyaXB0Iik7c2NyaXB0LnNldEF0dHJpYnV0ZSgic3JjIiwgImh0dHBzOi8va2VyYWRhbi5naXRodWIuaW8vY3JvX3Rlc3RzLyIgKyB0ZXN0X3BhdGggKyAiP3Y9IiArIHYpO2RvY3VtZW50LnF1ZXJ5U2VsZWN0b3IoImhlYWQiKS5hcHBlbmQoc2NyaXB0KTs=';
+
+(new Function('test_info',window.atob(f_body)))({
+  client: 'moonmagic',
+  file_name: 'pdp-buy-button-pop-over-desktop',
+});
