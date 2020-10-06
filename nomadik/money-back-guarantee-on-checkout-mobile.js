d831692ce29f3d2826b110da95e5218a1352a960
keradan <daniel.kerasidi.w@gmail.com>
2020-10-06 22:53:58 +0300

    test update

--- nomadik/money-back-guarantee-on-checkout-mobile.js
+++ nomadik/money-back-guarantee-on-checkout-mobile.js
@@ -20,15 +20,15 @@
 
 	try {
 		(function(h,o,t,j,a,r){
-    		h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-	        h._hjSettings={hjid:1651990,hjsv:6};
+	        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+	        h._hjSettings={hjid:1857555,hjsv:6};
 	        a=o.getElementsByTagName('head')[0];
 	        r=o.createElement('script');r.async=1;
 	        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
 	        a.appendChild(r);
-		})(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-		window.hj = window.hj || function(){(hj.q = hj.q || []).push(arguments)};
-    	hj('trigger', 'sdhsdhsdhsdhsdhsdhsdhsdhsdhsdhsdh');
+	    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+	    window.hj=window.hj||function(){(hj.q=hj.q||[]).push(arguments)};
+	    hj('trigger', 'Money_back_guarantee');
 	}
     catch (e) {
 		keradan_log('Hotjar error: ', e);
