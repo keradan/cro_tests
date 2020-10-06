c4ab7a2860eb38d268391f82d7c735bf4e957035
keradan <daniel.kerasidi.w@gmail.com>
2020-10-06 15:58:08 +0300

    test update

--- nomadik/money-back-guarantee-on-checkout-mobile.js
+++ nomadik/money-back-guarantee-on-checkout-mobile.js
@@ -38,11 +38,11 @@
  		.keradan-money-back-box {
 
  		}
- 		.krdnmbb .summary-box {
+ 		.krdnmbb .summary-box, .krdnmbb .shipping-schedule-box {
  			background: #F6F6F6;
  			padding: 10px 25px;
  		}
- 		.krdnmbb .summary-box .head {
+ 		.krdnmbb .summary-box .head, .krdnmbb .shipping-schedule-box .head {
  			position: relative;
  			display: flex;
 		    width: 100%;
@@ -51,12 +51,17 @@
 		    align-items: center;
 		    padding-right: 30px;
  		}
- 		.krdnmbb .summary-box .head .title {
+ 		.krdnmbb .summary-box .head .title, .krdnmbb .shipping-schedule-box .head .title {
  			font-family: Open Sans;
 		    font-style: normal;
 		    font-weight: 800;
 		    font-size: 16px;
 		    color: #282562;
+		}
+		.krdnmbb .shipping-schedule-box .head .title {
+			font-family: Roboto;
+			font-weight: bold;
+			font-size: 16px;
 		}
  		.krdnmbb .summary-box .head .price {
  			margin: 0;
@@ -66,14 +71,14 @@
 		    font-size: 16px;
 		    color: #212032;
  		}
- 		.krdnmbb .summary-box .head .arrow {
+ 		.krdnmbb .summary-box .head .arrow, .krdnmbb .shipping-schedule-box .head .arrow {
  			position: absolute;
 		    right: 0;
 		    width: 20px;
 		    transform: rotate3d(0, 0, 0, 0);
 		    transition: all 0.3s ease;
  		}
- 		.krdnmbb .summary-box.collapsed .head .arrow {
+ 		.krdnmbb .summary-box.collapsed .head .arrow, .krdnmbb .shipping-schedule-box.collapsed .head .arrow {
  			transform: rotate3d(180, 0, 0, 180deg);
  		}
  		.krdnmbb .summary-box .body {
@@ -87,7 +92,27 @@
 		    opacity: 1;
 		    transition: all 0.2s ease;
  		}
- 		.krdnmbb .summary-box.collapsed .body {
+ 		.krdnmbb .shipping-schedule-box .body {
+ 			margin: 0;
+		    margin-top: 10px;
+		    padding: 0;
+		    padding-top: 10px;
+		    max-height: 100vh;
+		    overflow: hidden;
+		    opacity: 1;
+		    transition: all 0.2s ease;
+		    color: #212032;
+		    font-family: Roboto;
+		    font-weight: normal;
+		    font-size: 14px;
+		    line-height: 18px;
+ 		}
+ 		.krdnmbb .shipping-schedule-box .body p {
+ 			margin: 0;
+ 			padding: 0;
+ 			margin-bottom: 10px;
+ 		}
+ 		.krdnmbb .summary-box.collapsed .body, .krdnmbb .shipping-schedule-box.collapsed .body {
 		    margin-top: 0;
 		    padding-top: 0;
 		    max-height: 0;
@@ -177,12 +202,18 @@
  	let exp_data = {
  		delete_icon: '<svg width="20" height="20" viewBox="0 0 20 20" fill="none"><path d="M10 20C4.477 20 0 15.523 0 10C0 4.477 4.477 0 10 0C15.523 0 20 4.477 20 10C20 15.523 15.523 20 10 20ZM10 18C12.1217 18 14.1566 17.1571 15.6569 15.6569C17.1571 14.1566 18 12.1217 18 10C18 7.87827 17.1571 5.84344 15.6569 4.34315C14.1566 2.84285 12.1217 2 10 2C7.87827 2 5.84344 2.84285 4.34315 4.34315C2.84285 5.84344 2 7.87827 2 10C2 12.1217 2.84285 14.1566 4.34315 15.6569C5.84344 17.1571 7.87827 18 10 18ZM10 8.586L12.828 5.757L14.243 7.172L11.414 10L14.243 12.828L12.828 14.243L10 11.414L7.172 14.243L5.757 12.828L8.586 10L5.757 7.172L7.172 5.757L10 8.586Z" fill="#ED143D"/></svg>',
  		summary_title: 'My Order Summary',
+ 		shipping_schedule_title: 'Shipping Schedule',
  		arrow: '<svg width="19" height="10" viewBox="0 0 19 10" fill="none"><path d="M1.1875 8.875L9.5 1.75L17.8125 8.875" stroke="black" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>',
  		check_mark: '<svg width="15" height="12" viewBox="0 0 15 12" fill="none"><path d="M5 8.586L1.707 5.293L0.292999 6.707L5 11.414L14.707 1.707L13.293 0.292999L5 8.586Z" fill="#32B259"/></svg>',
  		list: [
  			'Cancel subscription anytime inside your customer account',
  			'100% money-back guarantee',
  		],
+ 		shipping_schedule_body: `
+			<p>If you order before the 14th your box will ship by the 24th of this month.</p>
+			<p>If you order after the 14th your box ships next month by the 24th.</p>
+			<p>if you want it sooner, email us after you order <a href="mailto:info@thenomadik.com">info@thenomadik.com</a> we can expedite your first box for free.</p>
+ 		`,
  	};
 
  	let markup = `
@@ -195,6 +226,13 @@
  				<div class="arrow">${exp_data.arrow}</div>
  			</div>
  			<div class="body"></div>
+ 		</div>
+ 		<div class="shipping-schedule-box collapsed">
+ 			<div class="head">
+ 				<div class="title">${exp_data.shipping_schedule_title}</div>
+ 				<div class="arrow">${exp_data.arrow}</div>
+ 			</div>
+ 			<div class="body">${exp_data.shipping_schedule_body}</div>
  		</div>
  		<div class="container money-back-text">
 	 		<div class="column">
@@ -235,12 +273,13 @@
 
 	document.querySelectorAll('.cart_listing table.item_table .delete_column').forEach(function(delete_column){
 		let row = delete_column.parentElement;
-		// let form_id = delete_column.querySelector('form').getAttribute('id');
-		keradan_add_row_to_summary({
-			form_id: delete_column.querySelector('form').getAttribute('id'),
-			product_data: row.querySelector('td.product_column').innerHTML,
-			price: row.querySelector('td.price_column').textContent,
-		});
+		let form = delete_column.querySelector('form');
+		let product_column = row.querySelector('td.product_column');
+		let price_column = row.querySelector('td.price_column');
+		
+		if (!form || !product_column || !price_column) return;
+
+		keradan_add_row_to_summary({form_id: form.getAttribute('id'), product_data: product_column.innerHTML, price: price_column.textContent});
 	});
 
 	document.querySelector('.krdnmbb .summary-box .head').addEventListener('click', function(){
