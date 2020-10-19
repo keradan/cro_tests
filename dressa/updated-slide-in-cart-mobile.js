2975115f5d7439531f23555751bc2a25da58512d
keradan <daniel.kerasidi.w@gmail.com>
2020-10-19 16:00:12 +0300

    test update

--- dressa/updated-slide-in-cart-mobile.js
+++ dressa/updated-slide-in-cart-mobile.js
@@ -25,6 +25,8 @@
 		keradan_log('Hotjar error: ', e);
 	}
 
+	window.keradan_cart_iframe_status = null;
+
 	let markup_content = {
  		ddsjhdsjh: 'djdshjdsjh',
  		sksjskjaa: `dsjsdjhsdhj`,
@@ -41,6 +43,37 @@
 	document.querySelector('head').append(styles_el);
 
 	console.log('Test "Updated slide in cart - Mobile" is here');
+
+	function create_iframe() {
+		window.keradan_cart_iframe = document.createElement('iframe');
+		window.keradan_cart_iframe.classList.add('keradan-cart-iframe');
+		// window.keradan_cart_iframe.setAttribute('src', 'https://dressa.com.ua/cart');
+		window.keradan_cart_iframe.setAttribute('width', '350');
+		window.keradan_cart_iframe.setAttribute('height', '500');
+		window.keradan_cart_iframe.setAttribute('style', 'border: 2px solid red; margin-bottom: 100px;');
+		window.keradan_cart_iframe_status = 'created';
+		// window.keradan_cart_iframe_status = 'ready_to_use';
+		keradan_log('keradans cart iframe:', {
+			iframe: window.keradan_cart_iframe,
+			iframe_status: window.keradan_cart_iframe_status,
+		});
+	}
+
+	function run_iframe() {
+		keradan_log('run_iframe');
+
+		document.body.append(window.keradan_cart_iframe);
+
+		let keradan_cart_iframe_document = window.keradan_cart_iframe.contentWindow.document;
+
+		window.keradan_cart_iframe.contentWindow.document.open();
+		window.keradan_cart_iframe.contentWindow.document.write(document.documentElement.innerHTML);
+		window.keradan_cart_iframe.contentWindow.document.close();
+
+		// window.keradan_cart_iframe.contentWindow.document.querySelector('.link__shopping').click();
+		//window.keradan_cart_iframe.contentWindow.document.querySelector('.basket-btn app-dressa-button').click();
+		//window.keradan_cart_iframe.contentWindow.document.querySelectorAll('.counter__add')[1].click();
+	}
 
 
 
