b19e8b2968755a788b0cb2b0b8e37f2205e0a5c5
keradan <daniel.kerasidi.w@gmail.com>
2020-10-25 03:07:56 +0300

    test update

--- default/unencrypted_call.js
+++
@@ -1,5 +0,0 @@
-let test_path = test_info.client + "/" + test_info.file_name + ".js";
-let v = Math.floor(Date.now() / 1000);
-let script = document.createElement("script");
-script.setAttribute("src", "https://keradan.github.io/cro_tests/" + test_path + "?v=" + v);
-document.querySelector("head").append(script);
