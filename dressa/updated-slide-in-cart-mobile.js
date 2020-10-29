6824d4ca9718a15ac0995d3754c097481c893470
keradan <daniel.kerasidi.w@gmail.com>
2020-10-29 23:25:49 +0200

    test update

--- dressa/updated-slide-in-cart-mobile.js
+++ dressa/updated-slide-in-cart-mobile.js
@@ -10,11 +10,12 @@
 
 	// let oldXHROpen = window.XMLHttpRequest.prototype.open;
 	window.XMLHttpRequest.prototype.open = function(method, url, async, user, password) {
+		console.log('keradan window.XMLHttpRequest.prototype.open');
 		// do something with the method, url and etc.
-		this.addEventListener('load', function() {
-			// do something with the response text
-			console.log('keradan oldXHROpen load: ' + this.responseText);
-		});
+		// this.addEventListener('load', function() {
+		// 	// do something with the response text
+		// 	console.log('keradan oldXHROpen load: ' + this.responseText);
+		// });
 
 		return oldXHROpen.apply(this, arguments);
 	}
