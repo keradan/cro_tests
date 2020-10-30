4bed9fcefd5567334ac5e134f9b5fdd226af5535
keradan <daniel.kerasidi.w@gmail.com>
2020-10-30 11:11:42 +0200

    test update

--- dressa/updated-slide-in-cart-mobile.js
+++ dressa/updated-slide-in-cart-mobile.js
@@ -6,17 +6,16 @@
 	cur_test.init.enable_log = true;
 	cur_test.init.enable_ga_events = false;
 
-	let v = 11;
+	let v = 12;
 	cur_test.log(`%c Keradan's test "${cur_test.init.go_title}" (v - ${v}) is here:`, 'background: #222; color: #bada55',  cur_test);
 
 	let xhr_intercept_function = function() {
 		if (this.keradan_xhr_data.method == 'GET') return;
-		// 	try {
-		// 		parsed_body = JSON.parse(this.keradan_xhr_data.body);
-		// 		this.keradan_xhr_data.body = parsed_body;
-		// 	}
-	 //    	catch (e) { cur_test.log('keradan error when attempting to parse xhr body from json text: ', e); }
-		// }
+		try {
+			let parsed_body = JSON.parse(this.keradan_xhr_data.body);
+			this.keradan_xhr_data.body = parsed_body;
+		}
+    	catch (e) { cur_test.log('keradan error when attempting to parse xhr body from json text: ', e); }
 		
 		console.log('keradan xhr loaded: ', this.keradan_xhr_data);
 	}
