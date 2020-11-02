b259252d1b9be0d451040dc6c6e63063c05671d5
keradan <daniel.kerasidi.w@gmail.com>
2020-11-02 12:39:29 +0200

    test update

--- dressa/updated-slide-in-cart-mobile.js
+++ dressa/updated-slide-in-cart-mobile.js
@@ -5,7 +5,7 @@
 	// Set dev behavior:
 	cur_test.init.enable_log = true;
 	cur_test.init.enable_ga_events = false;
-	cur_test.init.debug_mode = true;
+	cur_test.init.debug_mode = false;
 
 	let v = 63;
 	cur_test.log(`%c Keradan's test "${cur_test.init.go_title}" (v - ${v}) is here:`, 'background: #222; color: #bada55',  cur_test);
