7a55dde6d03496f99c7aae401d0571dafd88552d
keradan <daniel.kerasidi.w@gmail.com>
2021-02-23 15:18:07 +0200

    test update

--- blackoakled/in-stock-banner.js
+++ blackoakled/in-stock-banner.js
@@ -5,10 +5,10 @@
 
 	// Set dev behavior:
 	cur_test.init.enable_log = true;
-	cur_test.init.enable_ga_events = false;
+	cur_test.init.enable_ga_events = true;
 	// cur_test.init.debug_mode = false;
 
-	let v = 3;
+	let v = 4;
 	cur_test.log(`%c Keradan's test "${cur_test.init.go_title}" (v - ${v}) is here:`, 'background: #222; color: #bada55',  cur_test);
 	cur_test.log(`%c Keradan's test script url:`, 'background: #222; color: #bada55',  document.currentScript.getAttribute('src'));
 
