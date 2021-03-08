8d8b4af493241edf008b5278f6a20f591dd99b7c
keradan <daniel.kerasidi.w@gmail.com>
2021-03-08 20:18:51 +0200

    test update

--- somnifix/new-pdp-mobile.js
+++ somnifix/new-pdp-mobile.js
@@ -8,19 +8,17 @@
 	cur_test.init.enable_ga_events = false;
 	// cur_test.init.debug_mode = false;
 
-	let v = 6;
+	let v = 7;
 	cur_test.log(`%c Keradan's test "${cur_test.init.go_title}" (v - ${v}) is here:`, 'background: #222; color: #bada55',  cur_test);
 	cur_test.log(`%c Keradan's test script url:`, 'background: #222; color: #bada55',  document.currentScript.getAttribute('src'));
-	
-	cur_test.log(`%c Keradan's test lalala`, 'background: #222; color: #bada55');
 
-	if (!document.querySelector(`#monthly-banner-long .month-sale-info`)) return;
+	// if (!document.querySelector(`#monthly-banner-long .month-sale-info`)) return;
 	
 	cur_test.ga_event('loaded');
 
 	// hotjar
 
-	cur_test.model = {
+	let model = cur_test.model = {
 		is_ready: false,
 		advantages: [
 			'Promote nose breathing',
@@ -98,7 +96,7 @@
 		},
 	};
 
-	let model = cur_test.model;
+	// let model = cur_test.model;
 
 	cur_test.html = document.createElement('div');
 	cur_test.html.classList.add(cur_test.init.css_scope_name);
@@ -208,12 +206,6 @@
 	// Шпора:
 	// console.log('keradan product__information: ', document.querySelectorAll('.product-template.product-main .product__information'));
 
-	setTimeout(function(){
-		// cur_test.log(`%c Keradan's deleyed log: `, 'background: #222; color: #bada55',  cur_test);
-		console.log('vse ok');
-		console.log('vse ok cur_test:', cur_test);
-	}, 5000);
-	
  	
 
 })();
