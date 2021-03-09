2c96c514372bb20a5532f3b21d337dffaf9e3771
keradan <daniel.kerasidi.w@gmail.com>
2021-03-09 04:01:12 +0200

    test update

--- somnifix/new-pdp-mobile.js
+++ somnifix/new-pdp-mobile.js
@@ -8,7 +8,7 @@
 	cur_test.init.enable_ga_events = false;
 	// cur_test.init.debug_mode = false;
 
-	let v = 38;
+	let v = 39;
 	cur_test.log(`%c Keradan's test "${cur_test.init.go_title}" (v - ${v}) is here:`, 'background: #222; color: #bada55',  cur_test);
 	cur_test.log(`%c Keradan's test script url:`, 'background: #222; color: #bada55',  document.currentScript.getAttribute('src'));
 	
@@ -235,6 +235,9 @@
 
 		cur_test.popups.packs.querySelectorAll('.pack').forEach(function(pack){
 			pack.addEventListener('click', function() {
+				cur_test.log('pack: ', pack);
+				cur_test.log('dataset.packId: ', dataset.packId);
+
 				model.set_pack(parseInt(pack.dataset.packId));
 				setTimeout(function(){
 					cur_test.close_popup(cur_test.popups.packs);
