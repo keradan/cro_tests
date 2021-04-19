58ebde906d8624f1c2bac3c37b60e579d0a19e44
keradan <daniel.kerasidi.w@gmail.com>
2021-04-20 00:27:53 +0300

    test update

--- get-licensed/course-search-step-flow.js
+++ get-licensed/course-search-step-flow.js
@@ -9,7 +9,7 @@ cur_test.init.event_category = 'Exp — Course search page';
 
 // Set dev behavior (for production need to comment out or remove):
 cur_test.init.enable_log = true;
-cur_test.init.enable_ga_events = false;
+cur_test.init.enable_ga_events = true;
 
 // Чтоб зафиксировать присутствие и версию
 cur_test.log(`%c Keradan's test "${cur_test.init.go_title}" (v - ${v}) is here:`, 'background: #222; color: #bada55', cur_test);
