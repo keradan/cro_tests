9564a6effa2a3ff70ba97536be8e60338eb626d7
keradan <daniel.kerasidi.w@gmail.com>
2021-04-22 01:44:28 +0300

    test update

--- get-licensed/course-search-step-flow.js
+++ get-licensed/course-search-step-flow.js
@@ -1,5 +1,5 @@
 // Версия чтоб понять загрузился ли на гитхаб или еще нет
-let v = 62;
+let v = 63;
 
 // Если IE тогда вместо currentScript будет так: document.querySelector('тут айдишник скрипта вставленный вручную')
 const cur_test = window.keradan.get_cur_test(document.currentScript);
